0883e26a56dbc044df9b655535220677ce81f27f
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-19 20:42:48 +0200

    adding provider and selector for state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 /** @format */
 
+import { useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+	const counter = useSelector((state) => state.counter);
 	return (
 		<div className="App">
-			<h1>Hello Word</h1>
+			<h1>Counter App</h1>
+			<h2>{counter}</h2>
+			https://youtu.be/zrs7u6bdbUw?list=PLyDE36G6PL48BO1vQoJGbpyWiGCtU0iNY&t=445
 		</div>
 	);
 }
