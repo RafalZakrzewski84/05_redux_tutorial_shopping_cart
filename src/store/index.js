d2a7333d6bec5263437420e564bf7e1f9b12ec19
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-20 13:34:16 +0200

    adding and configure store using redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,3 @@
 /** @format */
 
-import { createStore } from 'redux';
-
-const reducerFn = (state = { counter: 0 }, action) => {
-	//action for adding to counter
-	if (action.type === 'ADD') {
-		return { counter: state.counter + 1 };
-	}
-	//action for removing from counter
-	if (action.type === 'REM') {
-		return { counter: state.counter - 1 };
-	}
-	//action for adding 10 to counter
-	if (action.type === 'add10') {
-		//action payload defined in app.js
-		return { counter: state.counter + action.payload };
-	}
-	return state;
-};
-const store = createStore(reducerFn);
-
-export default store;
+import { configureStore, createSlice } from '@reduxjs/toolkit';
