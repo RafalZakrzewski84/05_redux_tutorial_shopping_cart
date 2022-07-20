84f438dd8d186f855e45613053dfede4f11bdf24
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-20 14:47:01 +0200

    adding and styling auth component

---
+++ src/Components/Auth.js
@@ -0,0 +1,21 @@
+/** @format */
+
+import React from 'react';
+import './Auth.css';
+
+function Auth() {
+	return (
+		<div className="Auth-Container">
+			<h1>Login</h1>
+			<form>
+				<label htmlFor="username">Username</label>
+				<input type="text" name="username" id="username" />
+				<label htmlFor="password">Password</label>
+				<input type="password" name="password" id="username" />
+				<button className="Auth-btn">Login</button>
+			</form>
+		</div>
+	);
+}
+
+export default Auth;
