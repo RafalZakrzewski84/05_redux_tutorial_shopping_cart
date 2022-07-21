83311b633e066c151a8208f649bfee744ba47c0c
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-21 09:19:19 +0200

    adding auth logic

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import { actions } from './store/index';
 import './App.css';
 
 import Auth from './Components/Auth';
+import Layout from './Components/Layout';
 
 function App() {
 	//initial state of auth.isLoggedIn from store from authSlice - will be false
 	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 	return (
 		<div className="App">
-			https://youtu.be/zrs7u6bdbUw?t=2103
-			<Auth />
+			{!isLoggedIn && <Auth />}
+			{isLoggedIn && <Layout />}
 		</div>
 	);
 }
--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -11,7 +11,7 @@ function Auth() {
 				<label htmlFor="username">Username</label>
 				<input type="text" name="username" id="username" />
 				<label htmlFor="password">Password</label>
-				<input type="password" name="password" id="username" />
+				<input type="password" name="password" id="password" />
 				<button className="Auth-btn">Login</button>
 			</form>
 		</div>
