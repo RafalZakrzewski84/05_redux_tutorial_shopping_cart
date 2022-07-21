ec6a97a5024d7af1fe0e9bca4f46716371abfcf3
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-21 11:53:36 +0200

    adding return in map function to show li tags for cartItem

--- src/Components/CartItems.js
+++ src/Components/CartItems.js
@@ -5,23 +5,23 @@ import { useSelector } from 'react-redux';
 import CartItem from './CartItem';
 import './Cart.css';
 const CartItems = () => {
-	const cartList = useSelector((state) => {
-		state.cart.itemList;
-	});
+	const cartList = useSelector((state) => state.cart.itemList);
 	return (
 		<div className="cart-container">
 			<h2>Your Cart</h2>
 			<ul>
 				{cartList.map((item, idx) => {
-					<li key={idx}>
-						<CartItem
-							id={item.id}
-							price={item.price}
-							name={item.name}
-							quantity={item.quantity}
-							total={item.totalPrice}
-						/>
-					</li>;
+					return (
+						<li key={idx}>
+							<CartItem
+								id={item.id}
+								price={item.price}
+								name={item.name}
+								quantity={item.quantity}
+								total={item.totalPrice}
+							/>
+						</li>
+					);
 				})}
 			</ul>
 		</div>
