df01d87bf1f6be4f4f431702c78f453378d45d37
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-21 11:03:05 +0200

    showing totalQuantity state in cart component

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,9 +1,13 @@
 /** @format */
 
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './Cart.css';
+
 const Cart = () => {
-	const quantity = 5;
+	//showing total items quantity in cart
+	const quantity = useSelector((state) => state.cart.totalQuantity);
+
 	return (
 		<div className="cartIcon">
 			<h3>Cart: {quantity} Items</h3>
