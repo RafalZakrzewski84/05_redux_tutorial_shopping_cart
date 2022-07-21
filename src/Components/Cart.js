860e4896595b1c83656dba83674eec784cb9195b
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-21 10:52:43 +0200

    adding cart component

---
+++ src/Components/Cart.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from 'react';
+import './Cart.css';
+const Cart = () => {
+	const quantity = 5;
+	return (
+		<div className="cartIcon">
+			<h3>Cart: {quantity} Items</h3>
+		</div>
+	);
+};
+
+export default Cart;
