f91b1b5781cf78e554b042b7b8b99661282c15b1
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-21 10:03:26 +0200

    structuring and styling layout component

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,9 +1,24 @@
 /** @format */
 
 import React from 'react';
+import Header from './Header';
+import Products from './Products';
+import './Layout.css';
+const Layout = () => {
+	let total = 100;
 
-function Layout() {
-	return <div>Layout</div>;
-}
+	return (
+		<React.Fragment>
+			<div className="layout">
+				<Header />
+				<Products />
+				<div className="total-price">
+					<h3>Total: ${total}</h3>
+					<button className="orderBtn">Place Order</button>
+				</div>{' '}
+			</div>
+		</React.Fragment>
+	);
+};
 
 export default Layout;
