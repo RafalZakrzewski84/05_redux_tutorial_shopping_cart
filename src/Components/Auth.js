4d12caf7fd81848626f60ef5abfb2e7a73dbaba4
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-21 09:34:00 +0200

    adding submit event and changing state of isLoggedIn

--- src/Components/Auth.js
+++ src/Components/Auth.js
@@ -2,12 +2,21 @@
 
 import React from 'react';
 import './Auth.css';
+import { useDispatch } from 'react-redux';
+import { authActions } from '../store/authSlice';
 
 function Auth() {
+	const dispatch = useDispatch();
+	//changing state of isLoggedIn
+	const submitHandler = (e) => {
+		e.preventDefault();
+		dispatch(authActions.login());
+	};
+
 	return (
 		<div className="Auth-Container">
 			<h1>Login</h1>
-			<form>
+			<form onSubmit={submitHandler}>
 				<label htmlFor="username">Username</label>
 				<input type="text" name="username" id="username" />
 				<label htmlFor="password">Password</label>
