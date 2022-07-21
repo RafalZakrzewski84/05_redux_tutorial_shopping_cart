468046ff15636335c28d5545dbc32408381235f1
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-21 10:38:45 +0200

    moving printing cart state to app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 /** @format */
 
-import { useSelector, useDispatch } from 'react-redux';
-import { actions } from './store/index';
+import { useSelector } from 'react-redux';
 import './App.css';
 
 import Auth from './Components/Auth';
@@ -10,6 +9,8 @@ import Layout from './Components/Layout';
 function App() {
 	//initial state of auth.isLoggedIn from store from authSlice - will be false
 	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+	const cartItems = useSelector((state) => state.cart.itemList);
+	console.log(cartItems);
 	return (
 		<div className="App">
 			{!isLoggedIn && <Auth />}
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,13 +1,11 @@
 /** @format */
 
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { cartActions } from '../store/cartSlice';
 
 import './Product.css';
 const Product = ({ name, id, imgURL, price }) => {
-	const cartItems = useSelector((state) => state.cart.itemList);
-	console.log(cartItems);
 	const dispatch = useDispatch();
 	const addToCart = () => {
 		dispatch(
