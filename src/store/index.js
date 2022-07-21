988dee3e58c346446de30e9c216d62d6865b6eb4
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-21 10:32:11 +0200

    importing cart state to store

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './authSlice';
+import cartSlice from './cartSlice';
 
 const store = configureStore({
 	reducer: {
 		auth: authSlice.reducer,
+		cart: cartSlice.reducer,
 	},
 });
 
