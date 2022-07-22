1c8ba626f25928cb2143a77524089c664226ee1f
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-22 11:58:43 +0200

    adding collecting response from fetch fn

--- src/App.js
+++ src/App.js
@@ -12,6 +12,26 @@ function App() {
 	//we have to log in
 	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
+	//checking state of cart from store - cartSlice
+	const cart = useSelector((state) => state.cart);
+
+	//effect will be induce in case of cart state change
+	React.useEffect(() => {
+		const sendRequestToFB = async () => {
+			const res =
+				await //connecting to firebase db for sending cart in json file
+				fetch(
+					'https://sdanews-acd6d-default-rtdb.europe-west1.firebasedatabase.app/cartItems.json',
+					{
+						method: 'PUT',
+						body: JSON.stringify(cart),
+					}
+				);
+			const data = await res.json();
+		};
+		sendRequestToFB();
+	}, [cart]);
+
 	return (
 		<div className="App">
 			{!isLoggedIn && <Auth />}
