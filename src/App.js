9bd3191bcc09e2a9e573189f238a79c19ee64daa
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-22 14:33:31 +0200

    adding logic for changing state of ui for notifictions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 /** @format */
 
 import * as React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { uiActions } from './store/uiSlice';
 import './App.css';
 
 import Auth from './Components/Auth';
 import Layout from './Components/Layout';
+import Notification from './Components/Notification';
+
+let isFirstRendered = true;
+console.log(isFirstRendered);
 
 function App() {
-	//initial state of auth.isLoggedIn from store from authSlice - will be false
-	//we have to log in
+	const dispatch = useDispatch();
+
+	const notification = useSelector((state) => state.ui.notification);
+
+	//initial state of auth.isLoggedIn from store from authSlice - will be false - we have to log in
 	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
 	//checking state of cart from store - cartSlice
@@ -17,10 +25,23 @@ function App() {
 
 	//effect will be induce in case of cart state change
 	React.useEffect(() => {
+		if (isFirstRendered) {
+			isFirstRendered = false;
+			console.log(isFirstRendered);
+			return;
+		}
 		const sendRequestToFB = async () => {
+			//ui in uiSlice - state before sending data
+			dispatch(
+				uiActions.showNotification({
+					open: true,
+					msg: 'Sending order...',
+					type: 'warning',
+				})
+			);
 			const res =
-				await //connecting to firebase db for sending cart in json file
-				fetch(
+				//connecting to firebase db for sending cart in json file
+				await fetch(
 					'https://sdanews-acd6d-default-rtdb.europe-west1.firebasedatabase.app/cartItems.json',
 					{
 						method: 'PUT',
@@ -28,12 +49,32 @@ function App() {
 					}
 				);
 			const data = await res.json();
+			// ui in uiSlice - state after successful putting data
+			dispatch(
+				uiActions.showNotification({
+					open: true,
+					msg: 'Data in database',
+					type: 'success',
+				})
+			);
 		};
-		sendRequestToFB();
+		sendRequestToFB().catch((e) => {
+			console.log(e);
+			dispatch(
+				uiActions.showNotification({
+					open: true,
+					msg: 'Something went wrong',
+					type: 'error',
+				})
+			);
+		});
 	}, [cart]);
 
 	return (
 		<div className="App">
+			{notification && (
+				<Notification type={notification.type} msg={notification.msg} />
+			)}
 			{!isLoggedIn && <Auth />}
 			{isLoggedIn && <Layout />}
 		</div>
