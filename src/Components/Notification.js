1620d2f3727f9a059f9aa40abef537ecb89174ee
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-22 14:32:02 +0200

    adding logic to component

--- src/Components/Notification.js
+++ src/Components/Notification.js
@@ -2,12 +2,31 @@
 
 import React from 'react';
 import Alert from '@mui/material/Alert';
+import { useSelector, useDispatch } from 'react-redux';
+import { uiActions } from '../store/uiSlice';
 
 //component will take type nad alert message as props
 const Notification = ({ type, msg }) => {
+	const dispatch = useDispatch();
+	const notification = useSelector((state) => state.ui.notification);
+
+	//function for closing Notification component
+	const closeHandler = () => {
+		dispatch(
+			uiActions.showNotification({
+				open: false,
+			})
+		);
+	};
+
 	return (
 		<div>
-			<Alert severity={type}>{msg}</Alert>
+			//showing alert when changing state of ui notification.open
+			{notification.open && (
+				<Alert onClick={closeHandler} severity={type}>
+					{msg}
+				</Alert>
+			)}
 		</div>
 	);
 };
